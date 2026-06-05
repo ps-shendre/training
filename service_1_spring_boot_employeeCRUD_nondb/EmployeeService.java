c90fea094e69fe3c233e8f93890b3594a3b3c9cf
ps-shendre <82023734+ps-shendre@users.noreply.github.com>
2026-06-05 09:33:31 +0530

    Add files via upload

---
+++ service_1_spring_boot_employeeCRUD_nondb/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.msedcl.main.service;
+
+import java.util.List;
+
+import com.msedcl.main.domain.Employee;
+
+public interface EmployeeService {
+	Employee addNewEmployee(Employee employee);
+	Employee updateEmployee(Employee employee);
+	Employee getEmployeeByEmployeeId(int employeeId);
+	List<Employee> getAllEmployees();
+	boolean deleteEmployeeByEmployeeId(int employeeId);
+}
---
+++ service_1_spring_boot_employeeCRUD_nondb/before using mapper and dto/EmployeeServiceImpl.java
@@ -0,0 +1,67 @@
+package com.msedcl.main.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.msedcl.main.entity.Employee;
+import com.msedcl.main.repository.EmployeeRepository;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+	private EmployeeRepository employeeRepository;
+
+	public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+		super();
+		this.employeeRepository = employeeRepository;
+	}
+
+	@Override
+	public Employee addNewEmployee(Employee employee) {
+		// TODO Auto-generated method stub
+		return employeeRepository.addNewEmployee(employee);
+	}
+
+	@Override
+	public Employee updateEmployee(Employee employee) {
+		// TODO Auto-generated method stub
+		return employeeRepository.updateEmployee(employee);
+	}
+
+	@Override
+	public Employee getEmployeeByEmployeeId(int employeeId) {
+
+		return employeeRepository.getEmployeeByEmployeeId(employeeId);
+	}
+
+	@Override
+	public boolean deleteEmployeeByEmployeeId(int employeeId) {
+		// TODO Auto-generated method stub
+		return employeeRepository.deleteEmployeeByEmployeeId(employeeId);
+	}
+
+	@Override
+	public List<Employee> getAllEmployee() {
+		// TODO Auto-generated method stub
+		return employeeRepository.getAllEmployee();
+	}
+
+	@Override
+	public List<Employee> getEmployeeByName(String name) {
+		// TODO Auto-generated method stub
+		return employeeRepository.getEmployeeByName(name);
+	}
+
+	@Override
+	public long getCountOfEmployees() {
+		// TODO Auto-generated method stub
+		return employeeRepository.getCountOfEmployees();
+	}
+
+	@Override
+	public List<Employee> getMaxSalary() {
+		// TODO Auto-generated method stub
+		return employeeRepository.getMaxSalary();
+	}
+
+}
